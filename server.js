13ce17d6af629b6a5095c615bcdee7941cf35937
Jiasen Wu <jiasenwu@hotmail.com>
2021-07-06 22:23:43 +0200

    add rewrite rules

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require('path')
 const { ArgumentParser } = require('argparse')
 const bodyParser = require("body-parser");
 const ExpressStaticS3 = require('express-static-s3')
+const rewrite = require('express-urlrewrite')
 
 const parser = new ArgumentParser({
     description: 'server process'
@@ -18,17 +19,15 @@ const INDEX_HTML = path.join(STATIC_PREFIX, 'index.html')
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-//initialize express-static-s3
 let staticServer=new ExpressStaticS3({
     accessKeyId:     process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
     bucket:          'thewonderfulwitchsu',
     prefix:          STATIC_PREFIX
 });
+app.use(rewrite('/', INDEX_HTML))
+app.use(rewrite('/assets/:name', path.join(STATIC_PREFIX, ':name'))
 app.use(staticServer.express)
-app.use('/', (req, res) => {
-    res.redirect(INDEX_HTML)
-})
 
 app.listen(args.port, '0.0.0.0',()=>{
     console.log('Server is on the port:', args.port)
