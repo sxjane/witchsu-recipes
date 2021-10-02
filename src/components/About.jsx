4ac90a22117ef7d95ff73a60a4f174a83343aae2
sxjane <sxj.jane@gmail.com>
2021-10-02 20:54:10 +0200

    add about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
 export default function About(){
-    return null
+    return <div style={{textAlign:'center', padding:'10rem'}}>
+    <h2 style={{color:'#003797'}}>苏苏和课代表们的小作文等待中</h2>
+</div>
 }
--- src/components/Market.jsx
+++ src/components/Market.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
 export default function Market(){
-    return null
+    return <div style={{textAlign:'center', padding:'10rem'}}>
+        <h2 style={{color:'#003797'}}>正在热火朝天的创造中</h2>
+    </div>
 }
