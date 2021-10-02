0fa688d63e352ed5e88474c73970304cccaaa27d
sxjane <sxj.jane@gmail.com>
2021-10-02 21:35:30 +0200

    add About

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function About(){
     return <div style={{textAlign:'center', padding:'10rem'}}>
-    <h2 style={{color:'#003797'}}>苏苏和课代表们的小作文等待中</h2>
+    <h2 style={{color:'#003797'}}>苏苏和课代表们的小作文在来的路上</h2>
 </div>
 }
